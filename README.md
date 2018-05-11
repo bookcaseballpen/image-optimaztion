896b96856c7a3e428ffdcc8e1fc7ce308aa55ef9
Sam Wang <sam_wang@creaf.com>
2018-05-11 14:27:34 +0800

    remove recursive in folder

--- README.md
+++ README.md
@@ -8,4 +8,11 @@ This tool can optimize png and jpg images for web usage.
 
 * place the folder in C Drive root
 * run the registrate with admin rights
+
+## Use
+
+* right click file and select Optimize Images
+  will generate optimized image in the same folder with additional -fs8 in file name.
+  recursive is not allowed for now.
 * right click folder and select Optimize Images
+  optimized image will be saved under the /optmz folder.
--- optimize.bat
+++ optimize.bat
@@ -1,13 +1,12 @@
 REM Turn off displaying actual commands 
 @echo off
-echo %1
 
 IF EXIST %1%/ (
     REM @echo %1
     cd %1
     md optmz
-    for /r %%i  in (*.jpg) do "C:\Image Optimization\cjpeg.exe" -quality 75 "%%i" > "%%~pi\optmz\%%~ni%%~xi" 
-    for /r %%i in (*.png) do "C:\Image Optimization\pngquant.exe" --force --verbose 256 "%%i" -o "%%~pi\optmz\%%~ni%%~xi"
+    for %%i  in (*.jpg) do "C:\Image Optimization\cjpeg.exe" -quality 75 "%%i" > "%%~pi\optmz\%%~ni%%~xi" 
+    for %%i in (*.png) do "C:\Image Optimization\pngquant.exe" --force --verbose 256 "%%i" -o "%%~pi\optmz\%%~ni%%~xi"
 
 ) ELSE (
     IF "%~x1" == ".png" (
@@ -19,4 +18,3 @@ IF EXIST %1%/ (
         "C:\Image Optimization\cjpeg.exe" -quality 75 "%1" >  "%~p1%~n1-fs8%~x1"  
     )
 )
-pause
